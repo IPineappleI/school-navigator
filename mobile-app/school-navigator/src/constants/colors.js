95a3f8167886c155c0da79aa5e700bf5d684c619
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2023-08-26 17:52:02 +0300

    Constants file rearrangement

---
+++ mobile-app/school-navigator/src/constants/colors.js
@@ -0,0 +1,3 @@
+export const HEADER_BACKGROUND = "#1849b3";
+
+export const CONTENT_BACKGROUND = "#ffffff";
--- mobile-app/school-navigator/src/constants/links.js
+++ mobile-app/school-navigator/src/constants/links.js
@@ -1,7 +1,3 @@
-export const HEADER_BACKGROUND = "#1849b3";
-
-export const CONTENT_BACKGROUND = "#ffffff";
-
 export const API_LINK = "http://192.168.50.229:5225";
 
 export const YANDEX_MAP_LINK = "https://yandex.ru/maps"
