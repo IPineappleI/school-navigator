131c45a75f8264839c52f0a32d9808e93179326f
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2023-08-26 17:52:58 +0300

    Added separated file for school tags

---
+++ mobile-app/school-navigator/src/constants/tags.js
@@ -0,0 +1,14 @@
+export const ProfileTags = [
+    { id: 0, key: "math", text: "Мат. класс", value: false, color: "#00ced1", image: require("../../assets/math-class.png")},
+    { id: 1, key: "it",  text: "ИТ класс", value: false, color: "#a4a0ff", image: require("..//..//assets/it-class.png")},
+    { id: 2, key: "engineering",  text: "Инж. класс", value: false, color: "#fae847", image: require("..//..//assets/engineering-class.png")},
+    { id: 3, key: "business",  text: "Бизнес класс", value: false, color: "#e38126", image: require("..//..//assets/bussiness-class.png")},
+];
+
+export const SortsTags = [
+    {id: 0, text: "Рейтингу", value: true, color: "#fef1c6", image: require("../../assets/rating.png")},
+    {id: 1, text: "Абитуриентам", value: false, color: "#97c6f1", image: require("../../assets/student.png")},
+    {id: 2, text: "Олимпиадникам", value: false, color: "#99f2b4", image: require("../../assets/medal.png")},
+    {id: 3, text: "Бюджетникам", value: false, color: "#e0e0e0", image: require("../../assets/budget.png")}
+]
+
